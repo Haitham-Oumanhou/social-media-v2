026445a951276659667259dcf6d7055426533c91
Haithaaam21 <oumanhou.haitham0123@gmail.com>
2023-06-04 14:03:21 +0100

    Profile added
    also changed some files name

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -4,12 +4,15 @@ import { Users } from "../../dummyData";
 import { useState } from "react";
 
 export default function Post({post}) {
-    console.log(post);
-    const [like,setLike]= useState(post.like);
-    const [isLiked ,setIsLiked]= useState(false);
-    const likeHandler =() =>{
-      setLike(isLiked ? like-1 :like+1 )
+    const [like,setLike] = useState(post.like)
+    const [isLiked,setIsLiked] = useState(false)
+  
+    const likeHandler =()=>{
+      setLike(isLiked ? like-1 : like+1)
+      setIsLiked(!isLiked)
     }
+   
+    
   return (
     <div className="post">
       <div className="postWrapper">
@@ -30,10 +33,10 @@ export default function Post({post}) {
           <img className="postImg" src={post?.photo} alt="" />
         </div>
         <div className="postBottom"> 
-          <div className="postBottomLeft">
+          <div className="postBottomLeft" >
             <img className="likeIcon" src="assets/like.png" onClick={likeHandler} alt="" />
-            {/* <img className="likeIcon" src="assets/heart.png" alt="" /> */}
-            <span className="postLikeCounter">{post.like} likes it</span>
+            {/* <img className="likeIcon" src="assets/heart.png" onClick={likeHandler} alt="" /> */}
+            <span className="postLikeCounter">{post.like}  likes it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{post.comment} comments</span>
